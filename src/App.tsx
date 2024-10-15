f4a81ee9ff18c82ad73868f00c47895b09ac6bab
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-10-15 20:13:21 -0300

    create coffee delivery structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { ThemeProvider } from 'styled-components'
 import './App.css'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello World!</h1>
-    </>
+      <button>13213</button>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+:focus {
+    outline: 0;
+    box-shadow: 0 0 0 1px ${props => props.theme['yellow-900']};
+}
+
+body {
+    background: ${props => props.theme.white};
+    color: ${props => props.theme['gray-700']};
+    -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+}
+`;
